bb4bd3049049d5f97494309bc3569c0aecb4f726
Renat <seashore@tutanota.de>
2025-10-05 13:40:48 +0500

    splitted parser function, json logger added

--- main.py
+++ main.py
@@ -1,100 +1,205 @@
 import requests
 import json
 import re
+import logging
+from pythonjsonlogger.json import JsonFormatter
+
+offer_limit = 20
+user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0"
+log_level = logging.DEBUG
+
+logger = logging.getLogger("kaspi_parser")
+logger.setLevel(log_level)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = JsonFormatter(
+    "{asctime}{msg}",
+    style="{",
+    rename_fields={"asctime": "time", "msg": "status"},
+    datefmt="%Y-%m-%dT%H:%M:%S"
+)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 product_item_pattern = re.compile(r"(?<=BACKEND\.components\.item = ){.+}(?=\s)")
+product_category_pattern = re.compile(r'(?<="category": ").+(?=")')
 
-with open("seed.json", "r") as f:
-    product = json.load(f)
+def parse_main_page(product_url):
+    headers = {"User-Agent": user_agent}
+    main_page = requests.get(product_url, headers=headers)
+    if main_page.status_code == 200:
+        logger.debug("success", extra={"action": "fetch_main_page", "url": main_page.url})
+    else:
+        logger.error("failed", extra={"action": "fetch_main_page", "url": main_page.url})
+        return None
+    try:    
+        product_item = json.loads(product_item_pattern.search(main_page.text).group(0))
+        product_id = product_item["card"]["id"]
+        product_name = product_item["card"]["title"]
+        product_category = product_category_pattern.search(main_page.text).group(0)
 
-product_url = product["product_url"]
+        product_specifications = product_item["specifications"]
+        new_product_specifications = {}
+        for sub_specification in product_specifications:
+            sub_specification_name = sub_specification["name"]
+            new_product_specifications[sub_specification_name] = {}
+            for feature in sub_specification["features"]:
+                feature_name = feature["name"]
+                feature_values = [x["value"] for x in feature["featureValues"]]
+                feature_str_values = ", ".join(feature_values)
+                new_product_specifications[sub_specification_name][feature_name] = feature_str_values
 
-main_page_headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0"
-}
+        del product_specifications
 
-main_page = requests.get(product_url, headers=main_page_headers)
+        product_images = product_item["galleryImages"]
+        logger.debug("success", extra={"action": "parse_main_page", "url": main_page.url})
+        return {
+            "id": product_id,
+            "name": product_name,
+            "category": product_category,
+            "specifications": new_product_specifications,
+            "images": product_images
 
-with open("main_page.html", "w") as f:
-    f.write(main_page.text)
+        } 
+    except AttributeError and KeyError:
+        logger.error("failed", extra={"action": "parse_main_page", "url": main_page.url})
+        return None
+    except Exception as e:
+        raise e
 
 
-result = {}
+def parse_reviews_page(product_id, product_url):
+    headers = {
+        "User-Agent": user_agent,
+        "Referer": product_url
+    }
 
-product_item = json.loads(product_item_pattern.search(main_page.text).group(0))
-product_id = product_item["card"]["id"]
-product_name = product_item["card"]["title"]
-product_category_id = product_item["card"]["categoryId"]
+    reviews_page = requests.get(f"https://kaspi.kz/yml/review-view/api/v1/reviews/product/{product_id}?withAgg=true", headers=headers)
+    if reviews_page.status_code == 200:
+        logger.debug("success", extra={"action": "fetch_reviews_page", "url": reviews_page.url})
+    else:
+        logger.error("failed", extra={"action": "fetch_reviews_page", "url": reviews_page.url})
+        return None
 
-product_specifications = product_item["specifications"]
-new_product_specifications = {}
-for sub_specification in product_specifications:
-    sub_specification_name = sub_specification["name"]
-    new_product_specifications[sub_specification_name] = {}
-    for feature in sub_specification["features"]:
-        feature_name = feature["name"]
-        feature_values = [x["value"] for x in feature["featureValues"]]
-        feature_str_values = ", ".join(feature_values)
-        new_product_specifications[sub_specification_name][feature_name] = feature_str_values
+    reviews = json.loads(reviews_page.text)
 
-product_images = product_item["galleryImages"]
+    try:
+        product_rating = reviews["summary"]["global"]
+        product_reviews_count = None
+        for group_count in reviews["groupSummary"]:
+            if group_count["id"] == "COMMENT":
+                product_reviews_count = group_count["total"]
+        logger.debug("success", extra={"action": "parse_reviews_page", "url": reviews_page.url})
+        return {
+            "rating": product_rating,
+            "reviews_count": product_reviews_count
+        }
+    except KeyError:
+        logger.error("failed", extra={"action": "parse_reviews_page", "url": reviews_page.url})
+        return None
+    except Exception as e:
+        raise e
 
-reviews_page_headers = main_page_headers.copy()
-reviews_page_headers.update({"Referer": product_url})
 
-reviews_page = requests.get(f"https://kaspi.kz/yml/review-view/api/v1/reviews/product/{product_id}?withAgg=true", headers=reviews_page_headers)
-reviews = json.loads(reviews_page.text)
+def parse_offers_page(product_id, product_url):
+    headers = {
+        "User-Agent": user_agent,
+        "Referer": product_url
+    }
+    post_data = {
+        "page": 0,
+        "limit": offer_limit,
+        "cityId": "710000000"
+    }
 
-product_rating = reviews["summary"]["global"]
-product_reviews_number = None
-for group_count in reviews["groupSummary"]:
-    if group_count["id"] == "COMMENT":
-        product_reviews_number = group_count["total"]
+    offers_page = requests.post(f"https://kaspi.kz/yml/offer-view/offers/{product_id}", headers=headers, json=post_data)
+    if offers_page.status_code == 200:
+        logger.debug("success", extra={"action": "fetch_offers_page", "url": offers_page.url})
+    else:
+        logger.error("failed", extra={"action": "fetch_offers_page", "url": offers_page.url})
 
-offers_request_data = {
-    "page": 0,
-    "limit": 20,
-    "cityId": "710000000"
-}
-
-offers_page = requests.post(f"https://kaspi.kz/yml/offer-view/offers/{product_id}", headers=reviews_page_headers, json=offers_request_data)
-offers = json.loads(offers_page.text)
-
-product_offers_number = offers["offersCount"]
-offers_pages_number = (product_offers_number // 20) + (product_offers_number % 20 != 0)
-
-product_offers = offers["offers"]
-for i in range(1, offers_pages_number):
-    offers_request_data["page"] = i
-    offers_page = requests.post(f"https://kaspi.kz/yml/offer-view/offers/{product_id}", headers=reviews_page_headers, json=offers_request_data)
+    # парсим первую страницу, берем данные об количество офферов,
+    # считаем сколько страниц
     offers = json.loads(offers_page.text)
-    product_offers.extend(offers["offers"])
+    try:
+        product_offers_number = offers["offersCount"]
+        # делит кол-во офферов на кол-во офферов на странице, округляет в большую сторону и выдает кол-во страниц
+        offers_pages_number = (product_offers_number // offer_limit) + (product_offers_number % offer_limit != 0)
+        product_offers = offers["offers"]
+        logger.debug("success", extra={"action": "parse_offers_page", "url": offers_page.url})
+    except KeyError:
+        logger.error("failed", extra={"action": "parse_offers_page", "url": offers_page.url})
+        return None
+    except Exception as e:
+        raise e
+    
+    # парсим остальные страницы
+    for i in range(1, offers_pages_number):
+        post_data["page"] = i
+        offers_page = requests.post(f"https://kaspi.kz/yml/offer-view/offers/{product_id}", headers=headers, json=post_data)
+        if offers_page.status_code == 200:
+            logger.debug("success", extra={"action": "fetch_offers_page", "url": offers_page.url})
+        else:
+            logger.error("failed", extra={"action": "fetch_offers_page", "url": offers_page.url})
+            return None
+        offers = json.loads(offers_page.text)
+        try:
+            product_offers.extend(offers["offers"])
+            logger.debug("success", extra={"action": "parse_offers_page", "url": offers_page.url})
+        except KeyError:
+            logger.error("failed", extra={"action": "parse_offers_page", "url": offers_page.url})
+            continue
+        except Exception as e:
+            raise e
+        
 
+    # выделяем только нужные данные
+    new_product_offers = []
+    for offer in product_offers:
+        new_offer = {}
+        new_offer["merchant_id"] = offer["merchantId"]
+        new_offer["merchant_name"] = offer["merchantName"]
+        new_offer["price"] = offer["price"]
+        new_product_offers.append(new_offer)
 
-new_product_offers = []
-for offer in product_offers:
-    new_offer = {}
-    new_offer["merchant_id"] = offer["merchantId"]
-    new_offer["merchant_name"] = offer["merchantName"]
-    new_offer["price"] = offer["price"]
-    new_product_offers.append(new_offer)
+    # список цен
+    prices = [x["price"] for x in new_product_offers]
 
+    product_min_price = min(prices)
+    product_max_price = max(prices)
 
-prices = [x["price"] for x in new_product_offers]
+    return {
+        "max_price": product_max_price,
+        "min_price": product_min_price,
+        "offers": new_product_offers
+    }
+    
 
-product_min_price = min(prices)
-product_max_price = max(prices)
+def kaspi_parser(product_url):
+    main_data = parse_main_page(product_url)
+    if main_data is None:
+        logger.error("failed", extra={"action": "fetch_product", "url":product_url})
+        return None
+    
+    reviews_data = parse_reviews_page(main_data["id"], product_url)
+    if reviews_data is not None:
+        main_data.update(reviews_data)
 
+    offers_data = parse_offers_page(main_data["id"], product_url)
 
-result["id"] = product_id
-result["name"] = product_name
-result["category_id"] = product_category_id
-result["specifications"] = new_product_specifications
-result["images"] = product_images
-result["rating"] = product_rating
-result["reviews_number"] = product_reviews_number
-result["offers_number"] = product_offers_number
-result["max_price"] = product_max_price
-result["min_price"] = product_min_price
+    logger.info("success", extra={"action": "fetch_product", "url":product_url})
 
-print(json.dumps(result, indent=4, ensure_ascii=False))
+    if offers_data is not None:
+        main_data.update(offers_data)
+        offers = main_data.pop("offers")
+        with open("export/offers.json", "w") as out:
+            json.dump(offers, out, indent=4, ensure_ascii=False)
+
+    with open("export/product.json", "w") as out:
+        json.dump(main_data, out, indent=4, ensure_ascii=False)
+
+if __name__ == "__main__":
+    with open("seed.json", "r") as f:
+        product = json.load(f)
+    product_url = product["product_url"]
+    kaspi_parser(product_url)
